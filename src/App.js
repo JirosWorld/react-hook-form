d5bc3d5bb6acad31e566cdb84f01e7eaf9d5584c
jolarti <jolarti@gmail.com>
2021-12-01 21:56:17 +0100

    uitwerkingen-opdracht-1

--- src/App.js
+++ src/App.js
@@ -1,68 +1,101 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
-
 import './App.css';
-import Textinput from "./components/Textinput";
-import Radioinput from "./components/Radioinput";
 
 function App() {
-    // const {register, handleSubmit, formState: {errors}} = useForm( {mode: "onChange"});
-    const { handleSubmit, formState: { errors }, register, watch } = useForm();
+    const { register, formState: { errors }, handleSubmit, watch } = useForm({
+        mode: 'onChange',
+    });
 
-    const selectedReferrer = watch('bezorgFrequentie');
+    const selectedFrequency = watch('delivery-frequency');
 
-    function onFormSubmit(data){
+    function onFormSubmit(data) {
         console.log(data);
     }
 
-  return (
-    <div className="container">
-        <form onSubmit={handleSubmit(onFormSubmit)}
-              id="mainform">
-            <Textinput name="Voornaam" register={register("Voornaam", {required: true})}/>
-            <Textinput name="Achternaam" register={register("Achternaam", {required: true})}/>
-            <Textinput name="Leeftijd" register={register("Leeftijd", {required: true})}/>
-            <Textinput name="Postcode" register={register("Postcode", {required: true})}/>
-
-            <label htmlFor="frequentie">
-                Bezorgfrequentie <br/>
-                <select id="frequentie" {...register("bezorgFrequentie", {required: true})}>
-                    <option value="iedere week">iedere week</option>
-                    <option value="om de week">om de week</option>
-                    <option value="iedere maand">iedere maand</option>
-                    <option value="other">Anders</option>
-                </select>
-            </label>
-
-            {/*conditineel veld, voeg 'watch' toe aan const*/}
-            {selectedReferrer === "other" &&  <input
+    return (
+        <form onSubmit={handleSubmit(onFormSubmit)}>
+            <label htmlFor="firstname-field">Voornaam:</label>
+            <input
                 type="text"
-                {...register("specificeer")}
-            />}
-
-
-            <label htmlFor="opmerkingen">
-                Opmerkingen <br/>
-                <textarea
-                    id="opmerkingen"
-                    rows="4"
-                    cols="30"
-                    {...register("opmerkingen")}
-                />
-            </label>
-            <label>
-                <br />
-                <input type="checkbox"
-                       {...register("checkbox", {required: true})}/>
-                Ik ga akkoord met de voorwaarden
-            </label>
-            <input type="submit"
-                //className={(errors.Achternaam && errors.Voornaam && errors.Leeftijd && errors.Postcode && errors.bezorgFrequentie && errors.radio && errors.checkbox) ? "none" : "redButton"}
+                id="firstname-field"
+                {...register("firstname", {
+                    required: "Voornaam is verplicht",
+                })}
             />
-            {errors.Voornaam && <p>geen voornaam</p>}
+            {errors.firstname && <p>{errors.firstname.message}</p>}
+
+            <label htmlFor="lastname-field">Achternaam:</label>
+            <input
+                type="text"
+                id="lastname-field"
+                {...register("lastname", {
+                    required: "Achternaam is verplicht",
+                })}
+            />
+            {errors.lastname && <p>{errors.lastname.message}</p>}
+
+            <label htmlFor="age-field">Leeftijd:</label>
+            <input
+                type="number"
+                id="age-field"
+                {...register("age", {
+                    required: "Leeftijd is verplicht",
+                    min: {
+                        value: 18,
+                        message: "Minimale leeftijd is 18",
+                    }
+                })}
+            />
+            {errors.age && <p>{errors.age.message}</p>}
+
+            <label htmlFor="zipcode-field">Postcode:</label>
+            <input
+                type="text"
+                {...register("zipcode", {
+                    required: "Postcode is verplicht",
+                    pattern: {
+                        value: /^[0-9]{4}[a-zA-Z]{2}$/,
+                        message: "Ongeldige postcode",
+                    }
+                })}
+            />
+            {errors.zipcode && <p>{errors.zipcode.message}</p>}
+
+            <label htmlFor="frequency-field">Bezorgfrequentie:</label>
+            <select {...register("delivery-frequency")}>
+                <option value="week">Iedere week</option>
+                <option value="two-week">Om de week</option>
+                <option value="month">Iedere maand</option>
+                <option value="other">Anders</option>
+            </select>
+            {selectedFrequency === "other" &&
+            <>
+                <label htmlFor="other-field">Specificeer:</label>
+                <input
+                    type="text"
+                    id="other-field"
+                    {...register("other")}
+                />
+            </>
+            }
+            <label htmlFor="comments-field">Opmerkingen:</label>
+            <textarea {...register("comments")} id="comments-field" cols="30" rows="10"></textarea>
+
+            <label htmlFor="terms-and-conditions-field">
+                <input
+                    type="checkbox"
+                    id="terms-and-conditions-field"
+                    {...register("terms-and-conditions")}
+                />
+                Ik ga akkoord met de algemene voorwaarden
+            </label>
+
+            <button type="submit">
+                Verzenden
+            </button>
         </form>
-    </div>
-  );
+    );
 }
 
 export default App;
